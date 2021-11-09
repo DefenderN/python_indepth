e6ea9ed4f9f7c8ed8ef78771113bf9daac889dde
DefenderN <nick.trapp@gmx.de>
2021-11-09 14:23:47 +0100

    Added unimportant stuff. fpar file shows mistakes!! Even afrter calling fixdut8ncfpar!

--- testfile.py
+++ testfile.py
@@ -1,6 +1,6 @@
 import nicksModule2 as nick
 
-# nick.makeOrthorombicSupercellFilesFromCommandLine()
+nick.makeOrthorombicSupercellFilesFromCommandLine()
 
 nick.addBBsToSupercellFile("orthorombicSupercell_3_3_3.mfpx",
                            "BBs/ZnPWL6.mfpx",
@@ -8,3 +8,8 @@ nick.addBBsToSupercellFile("orthorombicSupercell_3_3_3.mfpx",
                            "BBs/dabco.mfpx",
                            "BBs/ph-stub.mfpx",
                            "BBs/dabco-stub.mfpx")
+
+
+nick.assignParamsToMfpxFile("orthorombicSupercell_3_3_3_withBBs.mfpx")
+
+
